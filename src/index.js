3f9e4ac500e8fded683ebe3c94db479a83b3b218
Mike Marino <mich_marino@yahoo.com>
2022-02-06 17:45:23 -0500

    removed webvitals

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
