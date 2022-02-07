a3c38ca3e4fa97f0ef086f6d08d62758bb826db9
Mike Marino <mich_marino@yahoo.com>
2022-02-06 22:33:33 -0500

    trying web3 connect

--- src/App.js
+++ src/App.js
@@ -41,6 +41,10 @@ class App extends Component {
         const chainID = await web3.eth.net.getId();
         const walletName = this.getNetworkByName(chainID)
         console.log(walletName)
+
+        await window.ethereum.request({method: 'eth_requestAccounts'});    
+        //window.web3 = new Web3(window.ethereum);
+
         const accounts = await web3.eth.getAccounts()
         console.log(accounts)
         this.setState({wallet: walletName, account: accounts[0]})
