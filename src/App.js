bfb478dc6002299eb0753d12cbe5df067c514d46
Mike Marino <mich_marino@yahoo.com>
2022-02-06 21:38:03 -0500

    adding web3 import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-//import Web3 from 'web3'
+import Web3 from 'web3'
 import NavBar from "./components/navBar";
 import AppSliders from "./components/appSliders";
 
