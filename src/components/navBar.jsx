6bd9a21cb63e70f3637535b264ec66bb4f04c8e9
oniram307 <37271796+oniram307@users.noreply.github.com>
2022-02-07 21:18:51 -0500

    fixed title

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class NavBar extends Component {
     render() { 
         return (<nav className="navbar navbar-dark bg-dark px-3">
-        <span className="navbar-brand">My Dapp Store</span>
+        <span className="navbar-brand">DGen Guide</span>
         <form className="form-inline">
         <button className="btn btn-outline-success" type="button" onclick={this.props.onConnectWallet}>{this.props.wallet}</button>
         </form>
