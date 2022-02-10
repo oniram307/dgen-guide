cb534dfb4b6bff5149072b70122f4beedea1a099
oniram307 <37271796+oniram307@users.noreply.github.com>
2022-02-09 21:23:31 -0500

    Added logo.

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class NavBar extends Component {
     render() { 
         return (<nav className="navbar navbar-dark bg-dark px-3">
-        <span className="navbar-brand">DGen Guide</span>
+        <span className="navbar-brand"><img src="images/logo.png" /></span>
         <form className="form-inline">
         <button className="btn btn-outline-success" type="button" onclick={this.props.onConnectWallet}>{this.props.account}</button>
         </form>
