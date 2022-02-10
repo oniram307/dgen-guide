49fb783f2ab7162e93f4d33428d11e124a774431
oniram307 <37271796+oniram307@users.noreply.github.com>
2022-02-09 21:56:21 -0500

    logo fix

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Logo from './images/Logo.png';
 
 class NavBar extends Component {
     render() { 
         return (<nav className="navbar navbar-dark bg-dark px-3">
-        <a className="navbar-brand" href="http://dgen.guide"><img src="./images/logo.png" className="img-thumbnail bg-dark border-0" alt="..."/></a>
+        <a className="navbar-brand" href="http://dgen.guide"><img src={Logo} className="bg-dark border-0" alt="..."/></a>
         <form className="form-inline">
         <button className="btn btn-outline-success" type="button" onclick={this.props.onConnectWallet}>{this.props.account}</button>
         </form>
