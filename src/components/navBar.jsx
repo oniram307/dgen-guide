993b96a01b651d0346d4ad403bad35b657a10e4c
oniram307 <37271796+oniram307@users.noreply.github.com>
2022-02-09 22:05:59 -0500

    logo size

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -4,7 +4,7 @@ import Logo from '../images/logo.png';
 class NavBar extends Component {
     render() { 
         return (<nav className="navbar navbar-dark bg-dark px-3">
-        <a className="navbar-brand" href="http://dgen.guide"><img src={Logo} className="bg-dark border-0" width="50" height="50" alt="..."/></a>
+        <a className="navbar-brand" href="http://dgen.guide"><img src={Logo} className="bg-dark border-0" width="100" height="100" alt="..."/></a>
         <form className="form-inline">
         <button className="btn btn-outline-success" type="button" onclick={this.props.onConnectWallet}>{this.props.account}</button>
         </form>
