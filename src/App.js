762ffd377cedc7297be6718efca90316bebc28dc
oniram307 <37271796+oniram307@users.noreply.github.com>
2022-02-13 15:41:35 -0500

    First take at updated metamask interaction.

--- src/App.js
+++ src/App.js
@@ -40,33 +40,86 @@ class App extends Component {
     };
 
     async handleConnectWallet () {
+        const onboardButton = document.getElementById('connectButton');
+
         const web3 = new Web3(Web3.givenProvider)
         const chainID = await web3.eth.net.getId();
         const walletName = this.getNetworkByName(chainID)
         console.log(walletName)
 
         await window.ethereum.request({method: 'eth_requestAccounts'});    
-        //window.web3 = new Web3(window.ethereum);
 
         const accounts = await web3.eth.getAccounts()
         console.log(accounts)
+        onboardButton.innerText = accounts[0].substring(1,6) + "..." + accounts[0].slice(-4);
+
         this.setState({wallet: walletName, account: accounts[0]})
     };
 
-    componentDidMount() {
-         //this.handleConnectWallet()
-    };
+    componentDidMount (){
+
+        const onboardButton = document.getElementById('connectButton');
+        const forwarderOrigin = 'http://dgen.guide';
+
+        //Created check function to see if the MetaMask extension is installed
+        const isMetaMaskInstalled = () => {
+          //Have to check the ethereum binding on the window object to see if it's installed
+          const { ethereum } = window;
+          return Boolean(ethereum && ethereum.isMetaMask);
+        };
+      
+        //We create a new MetaMask onboarding object to use in our app
+        const onboarding = new MetaMaskOnboarding({ forwarderOrigin });
+      
+        //This will start the onboarding proccess
+        const onClickInstall = () => {
+          onboardButton = 'Onboarding in progress';
+          onboardButton.disabled = true;
+          //On this object we have startOnboarding which will start the onboarding process for our end user
+          onboarding.startOnboarding();
+        };
+      
+        const onClickConnect = async () => {
+          try {
+            // Will open the MetaMask UI
+            // You should disable this button while the request is pending!
+            await ethereum.request({ method: 'eth_requestAccounts' });
+          } catch (error) {
+            console.error(error);
+          }
+        };
+      
+        const MetaMaskClientCheck = () => {
+          //Now we check to see if Metmask is installed
+          if (!isMetaMaskInstalled()) {
+            //If it isn't installed we ask the user to click to install it
+            onboardButton.innerText = 'Install MetaMask!';
+            //When the button is clicked we call th is function
+            onboardButton.onclick = onClickInstall;
+            //The button is now disabled
+            onboardButton.disabled = false;
+          } else {
+            //If MetaMask is installed we ask the user to connect to their wallet
+            onboardButton.innerText = 'Connect Wallet';
+            //When the button is clicked we call this function to connect the users MetaMask Wallet
+            onboardButton.onclick = onClickConnectå;
+            //The button is now disabled
+            onboardButton.disabled = false;
+          }
+        };
+      
+        MetaMaskClientCheck();
+    }
 
     render () {
         let accountAddress = this.state.account;
 
         if (this.state.account !== "Connect Wallet") {
-            accountAddress = this.state.account.substring(1,6) + "..." + this.state.account.slice(-4);
         }
 
         return (
             <React.Fragment>
-            <NavBar account={accountAddress} onConnectWallet={this.handleConnectWallet}/>
+            <NavBar onConnectWallet={this.handleConnectWallet}/>
             <main className="container-fluid text-white bg-dark" >
                 <AppSliders wallet={this.state.wallet} appInventory={this.state.appInventory} />
             </main>
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -6,7 +6,7 @@ class NavBar extends Component {
         return (<nav className="navbar navbar-dark bg-dark px-3">
         <a className="navbar-brand" href="http://dgen.guide"><img src={Logo} className="bg-dark border-0" width="420" height="60" alt="..."/></a>
         <form className="form-inline">
-        <button className="btn btn-outline-success" type="button" onclick={this.props.onConnectWallet}>{this.props.account}</button>
+        <button id="connectButton" className="btn btn-outline-success" type="button" onclick={this.props.onConnectWallet}></button>
         </form>
       </nav>);
     }
