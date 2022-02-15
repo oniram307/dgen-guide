a73e347b2ff9833598c26b8408718a86a23da911
oniram307 <37271796+oniram307@users.noreply.github.com>
2022-02-14 21:17:17 -0500

    img-fluid change

--- src/components/appSlider.jsx
+++ src/components/appSlider.jsx
@@ -23,7 +23,7 @@ class AppSlider extends Component {
                         <div className="card text-white bg-dark text-center">
                             <div className="card-header pb-0">
                             <center>
-                            <img src={"https://" + app.appURL + "/favicon.ico"}  className="img-thumbnail bg-dark border-0" alt="..." height="100px"/>
+                            <img src={"https://" + app.appURL + "/favicon.ico"}  className="img-fluid img-thumbnail bg-dark border-0" alt="..." />
                             </center>
                             </div>
                             <div className="card-body pb-0">
