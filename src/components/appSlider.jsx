733d21627254a1d6e883fce80c0af8321f5b4553
oniram307 <37271796+oniram307@users.noreply.github.com>
2022-02-16 21:46:57 -0500

    changed to className

--- src/components/appSlider.jsx
+++ src/components/appSlider.jsx
@@ -22,7 +22,7 @@ class AppSlider extends Component {
                         adaptiveHeight={true} >
                     {filteredAppList.map(app => (
                         <div className="card text-white bg-dark text-center">
-                            <div class="corner-ribbon top-left sticky red shadow">Hello</div>
+                            <div className="corner-ribbon top-left sticky red shadow">Hello</div>
                             <div className="card-header pb-0">
                             <center>
                             <img src={"https://" + app.appURL + "/favicon.ico"}  className="img-fluid bg-dark border-0" alt="..." />
