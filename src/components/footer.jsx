089262c0a1c6d62d7e53ad67317f2e234f05c28d
oniram307 <37271796+oniram307@users.noreply.github.com>
2022-03-01 22:12:45 -0500

    footer update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() { 
         return (<nav className="navbar navbar-dark bg-dark px-3">
-        Test
+        <span class="navbar-brand mb-0 h1">Footerr</span>
       </nav>);
     }
 }
