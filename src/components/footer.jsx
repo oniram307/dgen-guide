ed57c48f2d14e3401023be99f2fa7810404a1c7f
oniram307 <37271796+oniram307@users.noreply.github.com>
2022-03-03 20:31:28 -0500

    trying another icon

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,6 +6,7 @@ class Footer extends Component {
         <span class="navbar-brand mb-0 h1">Footerr</span>
         <img src="https://img.icons8.com/plumpy/24/000000/github.png" alt=""/>
         <img src="https://img.icons8.com/plumpy/24/000000/twitter--v1.png" alt=""/>
+        <img src="https://img.icons8.com/ios/50/000000/twitter--v1.png" alt=""/>
       </nav>);
     }
 }
