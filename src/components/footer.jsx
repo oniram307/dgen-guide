7597b9d85b8955f5911583759772bad0b741a955
oniram307 <37271796+oniram307@users.noreply.github.com>
2022-03-03 20:34:04 -0500

    trying bg-light

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ class Footer extends Component {
         return (<nav className="navbar navbar-dark bg-dark px-3">
         <span class="navbar-brand mb-0 h1">Footerr</span>
         <img src="https://img.icons8.com/plumpy/24/000000/github.png" alt=""/>
-        <img src="https://img.icons8.com/plumpy/24/000000/twitter--v1.png" alt=""/>
+        <img className="bg-light" src="https://img.icons8.com/plumpy/24/000000/twitter--v1.png" alt=""/>
         <img src="https://img.icons8.com/ios/50/000000/twitter--v1.png" alt=""/>
       </nav>);
     }
