15a521708e11c4e434de0daf87c268abd50630c5
oniram307 <37271796+oniram307@users.noreply.github.com>
2022-03-03 20:21:33 -0500

    added alt

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,8 +4,8 @@ class Footer extends Component {
     render() { 
         return (<nav className="navbar navbar-dark bg-dark px-3">
         <span class="navbar-brand mb-0 h1">Footerr</span>
-        <img src="https://img.icons8.com/plumpy/24/000000/github.png"/>
-        <img src="https://img.icons8.com/plumpy/24/000000/twitter--v1.png"/>
+        <img src="https://img.icons8.com/plumpy/24/000000/github.png" alt=""/>
+        <img src="https://img.icons8.com/plumpy/24/000000/twitter--v1.png" alt=""/>
       </nav>);
     }
 }
