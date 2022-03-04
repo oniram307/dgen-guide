8a6269159c11addc140777be39bf35e9955d33b5
oniram307 <37271796+oniram307@users.noreply.github.com>
2022-03-03 20:42:26 -0500

    messing more with footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
     render() { 
-        return (<nav className="navbar navbar-dark bg-dark px-3">
+        return (<nav className="navbar navbar-light bg-dark px-3">
         <span class="navbar-brand mb-0 h1">Footerr</span>
         <img src="https://img.icons8.com/plumpy/24/000000/github.png" alt=""/>
-        <img className="bg-light" src="https://img.icons8.com/plumpy/24/000000/twitter--v1.png" alt=""/>
+        <img src="https://img.icons8.com/plumpy/24/000000/twitter--v1.png" alt=""/>
         <img src="https://img.icons8.com/ios/50/000000/twitter--v1.png" alt=""/>
       </nav>);
     }
